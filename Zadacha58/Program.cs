17d94c00d6fa30191ed597927841577d080fc336
Alexseiches <129062031+Alexseiches@users.noreply.github.com>
2023-12-01 14:49:30 +0700

    Add files via upload

---
+++ Zadacha58/Program.cs
@@ -0,0 +1,83 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[,] CreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+int[,] MultiplyArray(int[,] firstArr, int[,] secondArr)
+{
+    int newRow, newCol;
+    if (firstArr.GetLength(0) > secondArr.GetLength(0))
+    {
+        newRow = secondArr.GetLength(0);
+    }
+    else
+    {
+        newRow = firstArr.GetLength(0);
+    }
+    if (firstArr.GetLength(1) > secondArr.GetLength(1))
+    {
+        newCol = secondArr.GetLength(1);
+    }
+    else
+    {
+        newCol = firstArr.GetLength(1);
+    }
+
+    int[,] newArray = new int[newRow, newCol];
+
+    for (int i = 0; i < newRow; i++)
+    {
+        for (int j = 0; j < newCol; j++)
+        {
+            for (int e = 0; e < newRow; e++)
+            {
+                newArray[i, j] += firstArr[i, e] * secondArr[e, j];
+            }
+        }
+    }
+    return newArray;
+}
+
+int[,] firstArr = CreateArray(Prompt("Введите количество строк массива 1: "), Prompt("Введите количество столбцов массива1: "));
+int[,] secondArr = CreateArray(Prompt("Введите количество строк массива 2: "), Prompt("Введите количество столбцов массива2: "));
+Console.WriteLine("Первый массив:");
+FillArrayRandom(firstArr);
+PrintArray(firstArr);
+Console.WriteLine("Второй массив:");
+FillArrayRandom(secondArr);
+PrintArray(secondArr);
+Console.WriteLine("Результирующая матрица:");
+int[,] newArray = MultiplyArray(firstArr, secondArr);
+PrintArray(newArray);
---
+++ Zadacha60/Program.cs
@@ -0,0 +1,47 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[,,] CreateArray(int a, int b, int c)
+{
+    int[,,] array = new int[a, b, c];
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] FillArrayRandom(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(1, 10);
+            }
+        }
+    }
+    return array;
+}
+
+int[,,] newArr3D = CreateArray(Prompt("Input A: "), Prompt("Input B: "), Prompt("Input C: "));
+Console.WriteLine();
+FillArrayRandom(newArr3D);
+PrintArray(newArr3D);
